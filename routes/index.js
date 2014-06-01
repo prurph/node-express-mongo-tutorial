7479f6c1b57d44041249268e53f852e754cd0cfc
Prescott Murphy <prurph@gmail.com>
2014-05-31 20:29:00 -0400

    Add adduser functionality

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,48 @@ router.get('/helloworld', function(req, res) {
   res.render('helloworld', { title: 'Hello, World!' });
 });
 
+// GET Userlist page.
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({},{},function(e, docs) {
+    res.render('userlist', {
+      'userlist' : docs
+    });
+  });
+});
 module.exports = router;
+
+// GET New User page.
+router.get('/newuser', function(req, res) {
+  res.render('newuser', { title: 'Add New User' });
+});
+
+// POST to Add User Service
+router.post('/adduser', function(req, res) {
+
+  // Set our internal DB variable
+  var db = req.db;
+
+  // Get form values (from 'name' attrs)
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+
+  // Set collection
+  var collection = db.get('usercollection');
+
+  // Submit to DB
+  collection.insert({
+    'username' : userName,
+    'email' : userEmail
+  }, function(err, doc) {
+    if (err) {
+      res.send('There was a problem adding the information to the database.');
+    }
+    else {
+      // Change header so address bar doesn't say /adduser
+      res.location('userlist');
+      res.redirect('userlist');
+    }
+  });
+});
